e5f0e12ae260c8eec42b91059ab99dc8b4199e91
joaogil-git <61070209+joaogil-git@users.noreply.github.com>
2020-02-14 22:27:42 -0300

    Primeiros arquivos do Model

---
+++ Blog1/Models/Blog/Categoria/CategoriaEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog1.Models.Blog.Categoria
+{
+    public class CategoriaEntity
+    {
+        public string Nome;
+    }
+}
---
+++ Blog1/Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,16 @@
+﻿using Blog1.Models.Blog.Autor;
+using Blog1.Models.Blog.Categoria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog1.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        public string Titulo;
+        public AutorEntity Autor;
+        public CategoriaEntity Categoria;
+    }
+}
